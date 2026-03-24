bc2026f3a78cdf870021d7b6899a2aa77677e008
noe022 <noeliaperezmojica@gmail.com>
2026-03-24 01:13:33 +0000

    Categories localStorage, hover and icons

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const input = document.querySelector("#input input");
 const tasksDiv = document.querySelector("#tasks-div");
 const logo = document.querySelector("#logo");
 const ProgressBar = document.querySelector("#progress-bar");
-let tasksBackup = JSON.parse(localStorage.getItem("tasks"))|| [];
+let tasksBackup = JSON.parse(localStorage.getItem("tasks")) || [];
 tasksBackup.forEach(oneTask => createTask(oneTask));
 let firstEnter = tasksBackup.length > 0;
 const categories = document.querySelector("#categories");
@@ -18,6 +18,9 @@ const addTask = document.querySelector("#add-task");
 let subjectInput = document.querySelector("#subject-input");
 let nameSubject = document.querySelector("#name-subject");
 let subjectsList = document.querySelector("#subjects-list");
+let subjectsBackup = JSON.parse(localStorage.getItem("subjects")) || [];
+subjectsBackup.forEach(oneSubject => createSubject(oneSubject));
+let subjectBubble = document.querySelector(".subject-bubble");
 
 function TaskCardRemove(oneTask, taskCard){
   const copyArray = [];
@@ -100,10 +103,6 @@ input.addEventListener('keydown', function(event){
   }
 });
 
-logo.addEventListener('click', function(){
-  location.reload();
-});
-
 let isClicked = false;
 addTask.addEventListener('click', function(){
   if (!isClicked) {
@@ -115,12 +114,35 @@ addTask.addEventListener('click', function(){
   }
 }); 
 
+function createSubject(subjectValue) {
+  const subjectBubble = document.createElement('li');
+  subjectBubble.classList.add('subject-bubble');
+
+  const text = document.createElement('span');
+  subjectBubble.textContent = subjectValue;
+
+  const icons = document.createElement('div');
+  icons.classList.add('bubble-icons');
+  icons.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="gray"><path d="M186.67-186.67H235L680-631l-48.33-48.33-445 444.33v48.33ZM120-120v-142l559.33-558.33q9.34-9 21.5-14 12.17-5 25.5-5 12.67 0 25 5 12.34 5 22 14.33L821-772q10 9.67 14.5 22t4.5 24.67q0 12.66-4.83 25.16-4.84 12.5-14.17 21.84L262-120H120Zm652.67-606-46-46 46 46Zm-117 71-24-24.33L680-631l-24.33-24Z"/></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="gray"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+  `;
+  subjectBubble.appendChild(text);
+  subjectBubble.appendChild(icons);
+  subjectsList.appendChild(subjectBubble);
+}
+
 nameSubject.addEventListener('keydown', function(event){
   if (event.key === 'Enter') {
-    let subjectBubble = document.createElement('li');
-    subjectBubble.classList.add('subject-bubble');
-    subjectBubble.textContent = nameSubject.value;
-    subjectsList.appendChild(subjectBubble);
+    const subjectValue = nameSubject.value;
+    if (subjectValue == "") return;
+    createSubject(subjectValue);
+    subjectsBackup.push(subjectValue);
+    localStorage.setItem("subjects", JSON.stringify(subjectsBackup));
     nameSubject.value = "";
   }
+});
+
+logo.addEventListener('click', function(){
+  location.reload();
 });
